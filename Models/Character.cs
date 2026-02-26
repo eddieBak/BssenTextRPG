1731f1e802b49f5f3f5647468f0b128ec94dbc37
eddieBak <r2y1643@naver.com>
2026-02-26 18:52:02 +0900

    feat(player): Character 기본 추상 클래스 생성

---
+++ Models/Character.cs
@@ -0,0 +1,42 @@
+namespace TextRPG.Models;
+
+// 캐릭터 기본 추상 클래스
+public class Character
+{
+    #region 프로퍼티
+
+    public string Name { get; protected set; }
+    public int CurrentHp { get; protected set; }
+    public int MaxHp { get; protected set; }
+    public int CurrentMp { get; protected set; }
+    public int MaxMp { get; protected set; }
+    public int AttackPower { get; protected set; }
+    public int Defense { get; protected set; }
+    public int Level { get; protected set; }
+    // 생존 여부
+    public bool IsAlive => CurrentHp > 0;
+    #endregion
+
+    #region 생성자
+
+    protected Character(string name, int maxHp, int maxMp, int attackPower, int defense, int level)
+    {
+        Name = name;
+        MaxHp = maxHp;
+        CurrentHp = maxHp;
+        maxMp = maxMp;
+        CurrentMp = maxMp;
+        AttackPower = attackPower;
+        Defense = defense;
+        Level = level;
+    }
+
+
+    #endregion
+
+    #region 메서드
+
+
+
+    #endregion
+}
