ba5d728506a6848daed437e1852a62727fb05610
eddieBak <r2y1643@naver.com>
2026-02-26 20:52:55 +0900

    feat(player): 캐릭터 및 플레이어 정보 출력 메소드 추가

--- Data/GameManager.cs
+++ Data/GameManager.cs
@@ -60,7 +60,7 @@ public class GameManager
     private void CreateCharacter()
     {
         //이름 입력
-        Console.WriteLine("캐릭터의 이름을 입력하세요: ");
+        Console.Write("캐릭터의 이름을 입력하세요: ");
         string? name = Console.ReadLine(); // nullable 허용
         
         if (string.IsNullOrWhiteSpace(name))
@@ -106,10 +106,12 @@ public class GameManager
         Console.WriteLine($"\n{name}님, {job}직업으로 캐릭터가 생성되었습니다.");
         
         // 테스트 코드
-        Console.WriteLine($"Player HP:{Player.CurrentHp}");
-        Console.WriteLine($"Player MP:{Player.CurrentMp}");
-        Console.WriteLine($"Player ATK:{Player.AttackPower}");
-        Console.WriteLine($"Player DEF:{Player.Defense}");
+        // Console.WriteLine($"Player HP:{Player.CurrentHp}");
+        // Console.WriteLine($"Player MP:{Player.CurrentMp}");
+        // Console.WriteLine($"Player ATK:{Player.AttackPower}");
+        // Console.WriteLine($"Player DEF:{Player.Defense}");
+        
+        Player.DisplayInfo();
         
     }
     
