b25f313a41cd01c56b6c0818785d578208de59d2
eddieBak <r2y1643@naver.com>
2026-02-27 11:33:02 +0900

    feat(battle): 공격 및 피해 처리 메서드 추가
    - Player의 일반 공격 처리

--- Models/Enemy.cs
+++ Models/Enemy.cs
@@ -39,6 +39,7 @@ public class Enemy : Character
     }
     
     // 적 캐릭터 정보 출력
+
     public override void DisplayInfo()
     {
         // base.DisplayInfo();
@@ -47,6 +48,10 @@ public class Enemy : Character
         Console.WriteLine($"HP: {CurrentHp}/{MaxHp}");
         Console.WriteLine($"공격력: {AttackPower}");
         Console.WriteLine($"방어력: {Defense}");
+    }
+    public override int Attack(Character target)
+    {
+        return target.TakeDamage(AttackPower);
     }
 
     #endregion
